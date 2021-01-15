79e1fc1761e7ee174749b1939d5f2588d384586a
jessemarek <dethgrenade@gmail.com>
2021-01-15 09:18:59 -0800

    updated initial imports and project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { LoginPage } from './components/pages/LoginPage';
 
 const App = (): React.ReactElement => {
   return (
     <div className="App">
       <Switch>
         {/* Public Routes */}
+        <Route exact path="/" component={LoginPage} />
         {/* Private Routes */}
         {/* Fallback redirect to main app homepage! Change the 'to' property after setting up routes. */}
         <Route path="/" component={() => <Redirect to="/homepage" />} />
---
+++ src/components/pages/LoginPage/LoginContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RenderLogin from './RenderLogin';
+
+const Login = (): React.ReactElement => {
+  return <RenderLogin />;
+};
+
+export default Login;
